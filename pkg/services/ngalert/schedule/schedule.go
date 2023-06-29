dbff115a176979177324864863795ad00ac9dac4
Yuriy Tseretyan <yuriy.tseretyan@grafana.com>
2023-06-29 18:01:53 -0400

    add hysteresis expr command

--- pkg/services/ngalert/schedule/schedule.go
+++ pkg/services/ngalert/schedule/schedule.go
@@ -381,6 +381,8 @@ func (sch *schedule) ruleRoutine(grafanaCtx context.Context, key ngmodels.AlertR
 			dur = sch.clock.Now().Sub(start)
 			logger.Error("Failed to build rule evaluator", "error", err)
 		} else {
+			states := sch.stateManager.GetStatesForRuleUID(e.rule.OrgID, e.rule.UID)
+			ruleEval.Prepare(eval.PreviousState{ActiveResults: convertStatesToPreviousResults(states)})
 			results, err = ruleEval.Evaluate(ctx, e.scheduledAt)
 			dur = sch.clock.Now().Sub(start)
 			if err != nil {
@@ -577,4 +579,14 @@ func SchedulerUserFor(orgID int64) *user.SignedInUser {
 			},
 		},
 	}
+}
+
+func convertStatesToPreviousResults(s []*state.State) map[uint64]struct{} {
+	active := map[uint64]struct{}{}
+	for _, st := range s {
+		if st.State == eval.Alerting || st.State == eval.Pending {
+			active[st.ResultHash] = struct{}{}
+		}
+	}
+	return active
 }
